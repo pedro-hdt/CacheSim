1b728eb2f0832eac8cbed22b7c2b0356471eb5aa
Pedro Teixeira <pedro.h.d.teixeira@gmail.com>
2018-11-27 16:57:52 +0000

    Refactored and broke down access_cache function

--- mem_sim.c
+++ mem_sim.c
@@ -92,157 +92,180 @@ void print_statistics(uint32_t num_cache_tag_bits, uint32_t cache_offset_bits, r
     printf("Cache:hit-rate:%2.1f%%\n", cache_total_hits / (float)(cache_total_hits + cache_total_misses) * 100.0);
 }
 
-/*
- *
- * Add any global variables and/or functions here as needed.
- *
- */
-
-
-
 /*  
- *  data-type models for the constituents of the cache 
- *  these ignore the data part, as suggested, as it is
- *  beyond the point of the assignment
+ *  Data strucutre models for the building blocks of the cache. These ignore
+ *  the data in it, as suggested, as it is beyond the point of the assignment
  */
 
+// models a cache block
 typedef struct {
     uint32_t tag;
     uint32_t valid;
-    uint32_t last_access;
-    uint32_t added_on;
+    uint32_t last_access; // for LRU
+    uint32_t added_on;    // for FIFO
 } block_t;
 
-
+// models a set of cache blocks
 typedef struct {
-    uint32_t s_idx;
     block_t *blocks;
 } set_t;
 
-
+// encapsulates the pointer to the sets as a cache data structure
+/* I am aware this is unnecessary, but I think it helps conceptualise
+ *  and read the code, so I believe it is a good idea to keep it. */
 typedef struct {
     set_t *sets;
 } cache_t;
 
 
-uint32_t g_cache_index_bits = 0;
-uint32_t g_num_cache_sets = 0;
+/* Create a global variable for the cache so it can easily be 
+ * accessed in the different fucntions without passing a pointer
+ * around. */ 
 cache_t my_cache;
 
 
 uint32_t get_tag(uint32_t address) {
-    // create a bitmask to isolate the part of the address that corresponds to the tag
-    // we know the number of tag bits, so we can create a 32-bit max value
-    // and then shift left by the right amount
+    /* Create a bitmask to isolate the part of the address that corresponds
+     * to the tag. we know the number of tag bits, so we can create a 32-bit
+     * max value, and then shift left to introduce the 0's */
     uint32_t bitmask = 0xffffffff;
-    bitmask >>= 32 - g_num_cache_tag_bits;
     bitmask <<= 32 - g_num_cache_tag_bits;
-    // doing the bitwise AND operation, we get the tag
+    // doing the bitwise AND operation with the address, we get the tag
     uint32_t tag = address & bitmask;
+    /* Finally, we shift it right to have it as the smallest number possible,
+     * ie, in the least significant bits. This is because in a real cache only
+     * the minimum amount of bits is stored to save space */
     tag >>= 32 - g_num_cache_tag_bits;
     return tag;
 }
 
 uint32_t get_index(uint32_t address) {
+    /* To extract the index bits, a process similar to the tag is used.
+     * However, since these bits are in the middle, it requires the bitmask to
+     * be shifted twice until the right sequence of bits is reached */
     uint32_t bitmask = 0xffffffff;
-    bitmask <<= g_num_cache_tag_bits;
+    // 1 - Introduce 0's on the left to ignore the tag bits
     bitmask >>= g_num_cache_tag_bits;
+    // 2 - Introduce 0's on the right to ignore the offset bits
     bitmask >>= g_cache_offset_bits;
     bitmask <<= g_cache_offset_bits;
+    // 3 - Apply the mask
     uint32_t index = address & bitmask;
+    // Minimise number of bits being used, as explained in get_tag()
     index >>= g_cache_offset_bits;
     return index;
 }
 
+
+void replace_fifo(uint32_t  tag, set_t curr_set, uint32_t access_number) {
+    uint32_t first_in = curr_set.blocks[0].added_on;
+    uint32_t first_in_idx = 0;
+    // loop through the set, looking for the FIFO block, ie. the "oldest"
+    for (int i = 0; i < associativity; i++) {
+        block_t *curr_block = &curr_set.blocks[i];
+        /* if  the current block was added before the one that is to be
+         * evicted, set the curr one to be evicted instead */
+        if (curr_block->added_on < first_in) {
+            first_in = curr_block->added_on;
+            first_in_idx = i;
+        }
+    }
+    // Evict the block that has been in cache for the longest time
+    curr_set.blocks[first_in_idx].added_on = access_number;
+    curr_set.blocks[first_in_idx].tag = tag;
+    /* At this point in execution, all blocks are guaranteed to already
+     * have the valid bit already set, so there is no need to do that */
+}
+
+void replace_lru(uint32_t  tag, set_t curr_set, uint32_t access_number) {
+    uint32_t least_recent_access = curr_set.blocks[0].last_access;
+    uint32_t least_recent_access_idx = 0;
+    // loop through the set, looking for the LRU block
+    for (int i = 0; i < associativity; i++) {
+        block_t *curr_block = &curr_set.blocks[i];
+        /* if the last access to the current block happened before the oldest
+         * known access so far, set the current block to be evicted */
+        if (curr_block->last_access < least_recent_access) {
+            least_recent_access = curr_block->last_access;
+            least_recent_access_idx = i; 
+        }  
+    }
+    // Evict the block that has "been inactive" for the longest time
+    curr_set.blocks[least_recent_access_idx].last_access = access_number;
+    curr_set.blocks[least_recent_access_idx].tag = tag;
+    // Again, valid bit should already be set
+}
+
+void replace_random(uint32_t  tag, set_t curr_set) {
+    // Evict a random block within the set
+    uint32_t r = rand() % associativity;
+    curr_set.blocks[r].tag = tag;
+    // curr_set.blocks[r].valid = 1;
+    return;
+}
+
+
+void write_to_cache(uint32_t tag, set_t curr_set, uint32_t access_number) {
+    // Look for an invalid or empty block. If there is one, use it!
+    for (int i = 0; i < associativity; i ++) {
+        block_t *curr_block = &curr_set.blocks[i];
+        if (!curr_block->valid) {
+            curr_block->tag = tag;
+            curr_block->valid = 1;
+            curr_block->added_on = access_number;
+            curr_block->last_access = access_number;
+            return;
+        }
+    }
+    // If there is no available block, replace one, according to the policy
+    switch (replacement_policy) {
+        case FIFO:
+            replace_fifo(tag, curr_set, access_number);
+            break;
+        case LRU:
+            replace_lru(tag, curr_set, access_number);
+            break;
+        case Random:
+            replace_random(tag, curr_set);
+            break;
+    }
+    return;
+}
 
 
 void access_cache(uint32_t address, uint32_t access_number) {
     
     uint32_t tag = get_tag(address);
     uint32_t index = get_index(address);
+
+    /* For readability we introduce a current set. That is, the set
+     * the given address is conditioned to correspond to, given the
+     * associativity. Because the set struct points to the blocks, 
+     * rather than sotring them locally, we don't need to make this
+     * current set a pointer. */
     set_t curr_set = my_cache.sets[index];
 
     for (int i = 0; i < associativity; i++) {
-        block_t curr_block = curr_set.blocks[i];
-        if (curr_block.tag == tag && curr_block.valid) {
-            g_result.cache_hits++;
-            curr_block.last_access = access_number;
-            curr_set.blocks[i] = curr_block;
+        /* Again, for readability, we introduce a current block. However,
+         * this time it must be a pointer to block in the set, otherwise
+         * the changes would have no effect. */
+        block_t *curr_block = &curr_set.blocks[i];
+        /* On a hit: update last_access field (whether or not LRU is the
+         * replacement policy, since we would have to check ) */
+        if (curr_block->tag == tag && curr_block->valid) {
+            curr_block->last_access = access_number;
+            g_result.cache_hits++; // HIT!
             return;
         }
     }
 
-    g_result.cache_misses++;
+    // If execution reaches this point:
+    g_result.cache_misses++; // MISS!
 
-    if (replacement_policy == FIFO) {
-        /* keep track of which block to evict 
-        in case no empty space is found */
-        uint32_t first_in = curr_set.blocks[0].added_on;
-        uint32_t first_in_idx = 0;
-        /* loop through the set, looking for invalid data
-        that can be overwritten , while at the same time
-        looking for the FIFO block in case it is necessary */
-        for (int i = 0; i < associativity; i++) {
-            block_t curr_block = curr_set.blocks[i];
-            /* if curr_block was added before the one that
-            is to be evicted, set the curr one to be evicted
-            instead */
-            if (curr_block.added_on < first_in) {
-                first_in = curr_block.added_on;
-                first_in_idx = i;
-            }
-            // if there is free space, use it and finish this access
-            if (curr_block.valid == 0) {
-                curr_block.tag = tag;
-                curr_block.valid = 1;
-                curr_block.added_on = access_number;
-                curr_set.blocks[i] = curr_block;
-                return;
-            }
-        }
-        /* if execution reaches this point, there is no free space
-        so we evict the block that was first written */
-        // since it should already be valid, no need to set valid bit
-        curr_set.blocks[first_in_idx].added_on = access_number;
-        curr_set.blocks[first_in_idx].tag = tag;
-    }
-    else if (replacement_policy == LRU) {
+    // Finally, we write the requested block to cache
+    write_to_cache(tag, curr_set, access_number);
 
-        uint32_t least_recent_access = curr_set.blocks[0].last_access;
-        uint32_t least_recent_access_idx = 0;
-
-        for (int i = 0; i < associativity; i++) {
-            block_t curr_block = curr_set.blocks[i];
-            if (curr_block.last_access < least_recent_access) {
-                least_recent_access = curr_block.last_access;
-                least_recent_access_idx = i; 
-            }
-            if (curr_block.valid == 0) {
-                curr_block.tag = tag;
-                curr_block.valid = 1;
-                curr_block.last_access = access_number;
-                curr_set.blocks[i] = curr_block;
-                return;
-            }   
-        }
-        curr_set.blocks[least_recent_access_idx].last_access = access_number;
-        curr_set.blocks[least_recent_access_idx].tag = tag;
-    }
-
-    else {
-        for (int i = 0; i < associativity; i ++) {
-            block_t curr_block = curr_set.blocks[i];
-            if (curr_block.valid == 0) {
-                curr_block.tag = tag;
-                curr_block.valid = 1;
-                curr_set.blocks[i] = curr_block;
-                return;
-            }
-        }
-        uint32_t r = rand() % associativity;
-        curr_set.blocks[r].tag = tag;
-        curr_set.blocks[r].valid = 1;
-    }
+    return;
 }
 
 
@@ -315,14 +338,13 @@ int main(int argc, char** argv) {
     // TODO: check if all these variables need to be global!!!
     // Calculate appropriate number of bits for requested configuration
     g_cache_offset_bits = log2(cache_block_size);
-    g_num_cache_sets = number_of_cache_blocks / associativity;
-    g_cache_index_bits = log2(g_num_cache_sets);
-    g_num_cache_tag_bits = 32 - g_cache_index_bits - g_cache_offset_bits;
+    uint32_t num_cache_sets = number_of_cache_blocks / associativity;
+    uint32_t cache_index_bits = log2(num_cache_sets);
+    g_num_cache_tag_bits = 32 - cache_index_bits - g_cache_offset_bits;
 
-    my_cache.sets = malloc(g_num_cache_sets * sizeof(set_t));
+    my_cache.sets = malloc(num_cache_sets * sizeof(set_t));
     
-    for (int i = 0; i < g_num_cache_sets; i++) {
-        my_cache.sets[i].s_idx = i;
+    for (int i = 0; i < num_cache_sets; i++) {
         my_cache.sets[i].blocks = malloc(associativity * sizeof(block_t));
         for (int j = 0; j < associativity; j++) {
             my_cache.sets[i].blocks[j].valid = 0;
@@ -343,17 +365,17 @@ int main(int argc, char** argv) {
             break;
 
         /* Add your code here */
-
         access_cache(access.address, access_number);
 
         access_number++;
 
     }
     
-    for (int i = 0; i < g_num_cache_sets; i++) {
+    // free the allocated memory: first the pointer to each set of blocks
+    for (int i = 0; i < num_cache_sets; i++) {
         free(my_cache.sets[i].blocks);
     }
-
+    // then the pointer to the sets
     free(my_cache.sets);
 
     /* Do not modify code below. */
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 gcc -o mem_sim mem_sim.c -std=gnu99 -lm -g
 python3 exhauster.py
-diff exhaustive-report.txt exhaustive-pedro.txt
+diff exhaustive-report.txt exhaustive-pedro.txt &> diff.log
+wc -l diff.log
